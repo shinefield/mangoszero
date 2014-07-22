6443ce7fd173e449962848656c5b676bf8c03e6e
Daniel S. Reichenbach <daniel@kogitoapp.com>
2014-07-22 17:08:09 +0200

    [SQL] scripted events renamed.
    The table formerly known as `scripted_events_id` has been renamed
    to `scripted_events`. Simpler, more fitting. This fixes #9.
    
    Signed-off-by: Daniel S. Reichenbach <daniel@kogitoapp.com>

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -27,7 +27,7 @@
 #define __REVISION_SQL_H__
 
 #define REVISION_DB_CHARACTERS "required_z2410_s2063_12562_01_characters_various_tables"
-#define REVISION_DB_MANGOS "required_z2478_xxxxx_01_mangos_spell_affect"
+#define REVISION_DB_MANGOS "required_z2480_xxxxx_01_mangos_scripted_event"
 #define REVISION_DB_REALMD "required_10008_01_realmd_realmd_db_version"
 
 #endif // __REVISION_SQL_H__
