22c44380f592dd4c3a96f94f678dd4306aae1122
Daniel S. Reichenbach <daniel@kogitoapp.com>
2014-10-10 12:25:20 +0200

    [Eluna] backport required core changes
    Based on
    
    * https://github.com/ElunaLuaEngine/ElunaMangosClassic/commit/7da715a
    
    Signed-off-by: Daniel S. Reichenbach <daniel@kogitoapp.com>

--- src/game/Object.cpp
+++ src/game/Object.cpp
@@ -50,6 +50,7 @@
 #include "CreatureLinkingMgr.h"
 #include "Chat.h"
 #include "LuaEngine.h"
+#include "ElunaEventMgr.h"
 
 Object::Object()
 {
@@ -830,6 +831,7 @@ void Object::MarkForClientUpdate()
 }
 
 WorldObject::WorldObject() :
+    elunaEvents(new ElunaEventProcessor(this)),
     m_currMap(NULL),
     m_mapId(0), m_InstanceId(0),
     m_isActiveObject(false)
@@ -839,11 +841,17 @@ WorldObject::WorldObject() :
 WorldObject::~WorldObject()
 {
     Eluna::RemoveRef(this);
+    delete elunaEvents;
 }
 
 void WorldObject::CleanupsBeforeDelete()
 {
     RemoveFromWorld();
+}
+
+void WorldObject::Update(uint32 update_diff, uint32 /*time_diff*/)
+{
+    elunaEvents->Update(update_diff);
 }
 
 void WorldObject::_Create(uint32 guidlow, HighGuid guidhigh)
