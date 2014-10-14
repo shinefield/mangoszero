7cb1741e4b45891b4863f8922b38ea146809cd70
Daniel S. Reichenbach <daniel@kogitoapp.com>
2014-10-14 18:40:29 +0200

    [Core] added missing initialization
    Signed-off-by: Daniel S. Reichenbach <daniel@kogitoapp.com>

--- src/mangosd/MaNGOSsoap.h
+++ src/mangosd/MaNGOSsoap.h
@@ -85,7 +85,8 @@ class SOAPCommand
 {
     public:
         SOAPCommand():
-            pendingCommands(0, USYNC_THREAD, "pendingCommands")
+            pendingCommands(0, USYNC_THREAD, "pendingCommands"),
+            m_success(false)
         {
         }
         ~SOAPCommand()
