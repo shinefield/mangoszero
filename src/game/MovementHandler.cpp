4a038acb4fa3b5bc812cbfd8e96e2699c9b37865
Daniel S. Reichenbach <daniel@kogitoapp.com>
2014-11-10 07:38:36 +0100

    [Fix] resurrect players falling under the ground
    Signed-off-by: Daniel S. Reichenbach <daniel@kogitoapp.com>

--- src/game/MovementHandler.cpp
+++ src/game/MovementHandler.cpp
@@ -607,6 +607,9 @@ void WorldSession::HandleMoverRelocation(MovementInfo& movementInfo)
 
                 // cancel the death timer here if started
                 plMover->RepopAtGraveyard();
+
+                plMover->ResurrectPlayer(0.5f);
+                plMover->SpawnCorpseBones();
             }
         }
     }
